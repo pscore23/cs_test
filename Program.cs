507a10b7214224ccf213f968bcf6da0db68d1ab7
pscore23 <rubik2357x@gmail.com>
2022-09-27 21:01:45 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Program {
+    public class HelloWorld {
+        public static void Main(string[] args) {
+            string test = "Hello, World!";
+
+            Action<string> write = (text) => Console.WriteLine(text);
+
+            write(test);
+        }
+    }
+}
